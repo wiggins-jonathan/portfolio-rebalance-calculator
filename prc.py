13530b13a05b4ed0127b76b951dc7717dd442e2b
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2019-07-10 16:16:34 -0700

    Refactor app to use Alpha Vantage

--- prc.py
+++ prc.py
@@ -1,30 +1,36 @@
-import pandas_datareader.data as web
-from datetime import datetime as dt
+import yaml
+import pandas_datareader    as web
+from pathlib    import Path as path
 
-# Get current time
-pretty_date = '%Y-%m-%d'
-now         = (dt.now().strftime(pretty_date))
+# Get paths
+rel_path        = path(__file__).parent
+secrets_file    = path(f'{rel_path}/secrets.yaml')
+
+# Get Alpha Vantage api key
+with open(secrets_file, 'r') as f:
+    try:
+        api = yaml.safe_load(f)
+        key = api['alpha_vantage']['api_key']
+    except yaml.YAMLERROR as yaml_error:
+        print(yaml_error)
 
 stocks = input('Enter ticker symbols:\n')
 '''
- We need to decide how to parse user input here
- A few ways we could do this:
- 1). Split on comma
- 2). Split on space
- 3). Ask user how many tickers they need. Take in n tickers
- 4). Take in n tickers until we get a null ticker (the user hits enter without
-    specifying a anything
- 5). Some other ways I can't think of right now
+ We need to decide how to parse user input here.
+ A few ways we could do this in rising levels of difficulty:
+    1). Split on delimmiter <- We are doing this now using a comma. It sucks & is lazy
+    2). Ask user how many tickers they need. Take in n tickers. This kind of sucks,
+       But would work quickly.
+    4). Loop through multiple inputs & take in n tickers until we get a null ticker,
+       (the user hits enter without specifying anything)
+    5). We take in command line arguments using the argparse module.
+    6). We create an ncurses TUI
+    7). We create a webapp
 '''
-# Print closing price
-#df = web.DataReader(stocks, 'stooq')
-#close = (df.loc[now, 'Close'])
-#print(close)
 
 def get_close_price(stocks):
     stocks = stocks.split(', ')
-    df = web.DataReader(stocks, 'stooq')
-    close = (df.loc[now, 'Close'])
-    print(close)
+    df = web.av.quotes.AVQuotesReader(symbols=stocks, api_key=key)
+    print(df.read())
 
 get_close_price(stocks)
