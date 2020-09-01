1cdbcd363b57c15865c828173b0a11e78b5b6eb5
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2020-09-01 00:27:41 -0700

    Rewite prc using object-oriented paradigm. Hopefully this will help with an easier understanding of the math

--- prc.py
+++ prc.py
@@ -4,35 +4,52 @@ import  arguments
 import  logging
 import  sys
 import  yfinance as yf
-from    pathlib import Path as path
 
 log = logging.getLogger(__name__)   # Instantiate logger object
 
-def main():
-    args = arguments.get_args()
-    _check_debug(args)
+class Ticker:
+    def __init__(self, name, amount, desiredPercent):
+        self.name           = name
+        self.amount         = amount
+        self.desiredPercent = desiredPercent
 
-    tickers = (args["assets"][0])
+        # Get today's closing price
+        try:
+            log.debug(f'Getting closing price for {self.name} from yf')
+            df = yf.download(               # yf returns a pandas dataframe
+                    self.name,
+                    period      = "1d",     # 1 day
+                    auto_adjust = True,     # Adjust for stock splits
+                    progress    = False,    # Prints download progress to stdout
+                    threads     = True      # Multi-thread download of ticker
+                    )
 
-    log.debug('Rounding final totals to 2 significant figures...')
-    totals = _round_list(_get_final_totals(args),   2)
-    shares = _round_list(_get_share_amounts(args),  0)
+            self.price = df["Close"][0]
 
-    for z, y, x in zip(shares, tickers, totals):
-        if x < 0:
-            x   = abs(x)
-            z   = abs(z)
-            var = 'Sell'
-        else:
-            var = 'Buy'
+            log.debug(f'Closing price for {self.name} is {self.price}.')
+        except:
+            print("Downloading data using yfinance failed. They probably changed their API.")
+            sys.exit(1)
 
-        print(f'{var} ${x} of {y} or about {z} share(s)')
+    # I think we can have a doMath function that will take in anonymous functions (lambda),
+    # register the answer with the Ticker object, & also return the answer
+    def sharesToBuy(self, sum_total, cash_addition):
+        self.actualPercent  = (self.amount / sum_total) * 100
+        log.debug(f'{self.name} is {self.actualPercent}% of total portfolio.')
 
-#
-## Helper functions
-#
+        self.percentDiff    = self.desiredPercent - self.actualPercent
+        log.debug(f'Percent difference from desired percent is {self.percentDiff}%')
 
-# Print debug statements to console if debug mode is on
+        self.targetPercent  = self.percentDiff + self.desiredPercent
+        log.debug(f'Percent to add to equal desired percent is {self.targetPercent}%')
+
+        self.amountToChange = (self.targetPercent * cash_addition) / 100
+        log.debug(f'Need to buy ${self.amountToChange} worth of {self.name} to equal desired percentage.')
+
+        self.sharesToBuy    = self.amountToChange / self.price
+        log.debug(f'Need to buy {self.sharesToBuy} shares of {self.name} to equal desired portfolio.')
+        return self.sharesToBuy
+
 def _check_debug(args):
     if args['debug'] == 1:
         logging.basicConfig(
@@ -40,93 +57,34 @@ def _check_debug(args):
             format  = '%(levelname)s - %(message)s'
         )
 
-def _get_share_price(args):
-    stocks = args["assets"]
+def main():
+    args = arguments.get_args()
+    _check_debug(args)
 
-    # yf.download expects a white space separated string of tickers
-    tickers = " ".join(stocks[0])
-    try:
-        df = yf.download(
-            tickers,
-            period      = "1d",
-            auto_adjust = True,
-            progress    = False,
-            threads     = True
-        )
+    # Data is in a list of lists at the 0th index
+    tickers     = args["assets"][0]
+    amounts     = args["amounts"][0]
+    percents    = args["percents"][0]
+    sum_total   = sum(amounts)
 
-        prices = []
-        for stock in stocks[0]:
-            prices.append(df["Close"][stock][0])
+    # Instantiate all ticker objects
+    for i in range(len(tickers)):
+        t           = Ticker(tickers[i], amounts[i], percents[i])
+        sharesToBuy = t.sharesToBuy(sum_total, args["total"])
 
-        log.debug(f'Successfully retrieved closing prices from yahoo finance {prices}')
+        # Round share & dollar amounts to 2 sig figs
+        sharesToBuy     = round(sharesToBuy, 2)
+        amountToChange  = round(t.amountToChange, 2)
 
-        return(prices)
-    except:
-        print("Downloading data using yfinance failed. They probably changed their API.")
-        sys.exit(1)
+        # Display if we should buy or sell
+        if sharesToBuy < 0:
+            sharesToBuy = abs(sharesToBuy)
+            var = "Sell"
+        else:
+            var = "Buy"
+        print(f'{var} ${amountToChange} of {t.name} or about {sharesToBuy} shares')
 
-def _get_share_amounts(args):
-    prices = _get_share_price(args)
-    totals = _get_final_totals(args)
 
-    share_amounts = [y / x for y, x in zip(totals, prices)]
-
-    return(share_amounts)
-
-def _get_amount_totals(args):
-    amount_args = args["amounts"][0]
-    total       = sum(amount_args)
-
-    return(total)
-
-def _get_actual_percentages(args):
-    percent_args    = args["amounts"][0]
-    actual_percents = []
-
-    for percent in percent_args:
-        totals = 100 * (percent / _get_amount_totals(args))
-        actual_percents.append(totals)
-
-    return(actual_percents)
-
-def _get_target_percents(args):
-    desired = args["percentages"][0]
-    actual  = _get_actual_percentages(args)
-
-    diffs           = []
-    target_percents = []
-
-    diffs           = [y - x for y, x in zip(desired, actual)]
-    target_percents = [y + x for y, x in zip(diffs, desired)]
-
-    return(target_percents)
-
-def _get_final_totals(args):
-    total   = args["total"]
-    targets = _get_target_percents(args)
-
-    final_totals = []
-
-    for target in targets:
-        final_total = (target * total) / 100
-        final_totals.append(final_total)
-
-    return(final_totals)
-
-def _round_list(list_to_round, sig_fig):
-    totals = list_to_round
-
-    rounded_totals = []
-
-    for total in totals:
-        rounded_total = round(total, sig_fig)
-        rounded_totals.append(rounded_total)
-
-    return(rounded_totals)
-
-#
-## End Helper Functions
-#
 
 if __name__ == "__main__":
     main()
