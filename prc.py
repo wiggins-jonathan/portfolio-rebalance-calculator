258676461714a9eedb03ad049c7c8a9d02e1dc43
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2020-09-13 13:34:33 -0700

    Users with lots of inputs can be quickly overwhelmed by the amount of args required. Change to an interactive prompt to avoid this problem.

--- prc.py
+++ prc.py
@@ -18,30 +18,30 @@ class Ticker:
             log.debug(f'Getting closing price for {self.name} from yf')
             df = yf.download(               # yf returns a pandas dataframe
                     self.name,
-                    period      = "1d",     # 1 day
+                    period      = '1d',     # 1 day
                     auto_adjust = True,     # Adjust for stock splits
                     progress    = False,    # Prints download progress to stdout
                     threads     = True      # Multi-thread download of ticker
                     )
 
-            self.price = df["Close"][0] # Grab closing price out of data frame
+            self.price = df['Close'][0] # Grab closing price out of data frame
 
             log.debug(f'Closing price for {self.name} is {self.price}.')
         except:
-            print("Downloading data using yfinance failed. They probably changed their API.")
+            print('Downloading data using yfinance failed.')
             sys.exit(1)
 
     # I think we can have a doMath function that will take in anonymous functions (lambda),
     # register the answer with the Ticker object, & also return the answer
-    def sharesToBuy(self, sum_total, cash_addition):
-        self.actualPercent  = (self.amount / sum_total) * 100
+    def sharesToBuy(self, sum_amounts, cash_addition):
+        self.actualPercent  = (self.amount / sum_amounts) * 100
         log.debug(f'{self.name} is {self.actualPercent}% of total portfolio.')
 
         self.percentDiff    = self.desiredPercent - self.actualPercent
         log.debug(f'Percent difference from desired percent is {self.percentDiff}%')
 
         self.targetPercent  = self.percentDiff + self.desiredPercent
-        log.debug(f'Percent to add to equal desired percent is {self.targetPercent}%')
+        log.debug(f'Percent of cash addition to add to equal desired percent is {self.targetPercent}%')
 
         self.amountToChange = (self.targetPercent * cash_addition) / 100
         log.debug(f'Need to buy ${self.amountToChange} worth of {self.name} to equal desired percentage.')
@@ -58,32 +58,48 @@ def _check_debug(args):
             format  = '%(levelname)s - %(message)s'
         )
 
+# Reads input continuously until no input received.
+# Returns a generator for each input loop
+def readInput(prompt):
+    x = input(prompt)
+    while x:
+        yield x
+        x = input(prompt)
+
 def main():
     args = arguments.get_args()
     _check_debug(args)
 
-    # Data is in a list of lists at the 0th index
-    tickers     = args["assets"][0]
-    amounts     = args["amounts"][0]
-    percents    = args["percents"][0]
-    sum_total   = sum(amounts)
+    tickers = list(readInput('Enter ticker symbols -> '))
+
+    amounts, percents = [], []
+    for ticker in tickers:
+        prompt = f'Enter current amount in portfolio for {ticker} -> '
+        amounts.append(float(input(prompt)))
+
+        prompt = f'Enter desired percentage of {ticker} in portfolio -> '
+        percents.append(float(input(prompt)))
+
+    sum_amounts = sum(amounts)
+
+    total = float(input('Enter total amount being contributed to porfolio -> '))
 
     # Instantiate all ticker objects
     for i in range(len(tickers)):
         t           = Ticker(tickers[i], amounts[i], percents[i])
-        sharesToBuy = t.sharesToBuy(sum_total, args["total"])
+        sharesToBuy = t.sharesToBuy(sum_amounts, total)
 
         # Round share & dollar amounts to 2 sig figs
         sharesToBuy     = round(sharesToBuy, 2)
         amountToChange  = round(t.amountToChange, 2)
 
         # If amount is negative we need to sell, not buy
-        var = "Buy"
+        var = 'Buy'
         if sharesToBuy < 0:
             sharesToBuy = abs(sharesToBuy)
-            var = "Sell"
+            var = 'Sell'
 
         print(f'{var} ${amountToChange} of {t.name} or about {sharesToBuy} shares')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
